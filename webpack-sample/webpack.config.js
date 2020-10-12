bd2fe77992a790a9e6a841f74f819528c102b034
fukunagaa <k.k.0627ku@gmail.com>
2020-10-12 22:52:08 +0900

    設定ファイルの追加

---
+++ webpack-sample/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+
+module.exports = {
+  // モード値を production に設定すると最適化された状態で、
+  // development に設定するとソースマップ有効でJSファイルが出力される
+  mode: "development",
+  entry: {
+    app: path.join(__dirname, "src", "app.ts"),
+  },
+  // import 文で .ts ファイルを解決するため
+  // これを定義しないと import 文で拡張子を書く必要が生まれる。
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+      },
+    ],
+  },
+  // ES5(IE11等)向けの指定（webpack 5以上で必要）
+  target: ["web", "es5"],
+};

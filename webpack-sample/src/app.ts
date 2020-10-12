16c4ea00cae3ce4fa486c16babad697f6d73252b
fukunagaa <k.k.0627ku@gmail.com>
2020-10-12 23:00:41 +0900

    app.js サンプルコード追加

---
+++ webpack-sample/src/app.ts
@@ -0,0 +1,18 @@
+// String
+const Hello = (name: string): string => {
+  return "Hello " + name;
+};
+
+// Number
+const Add = (num1: number, num2: number): number => {
+  return num1 + num2;
+};
+
+// void
+const Self = (lan: string): void => {
+  console.log("your select: " + lan);
+};
+
+console.log(Hello("man!"));
+console.log(Add(3, 5));
+Self("woman");

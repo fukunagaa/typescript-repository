a081e4e47ff80a95c6c9035d37d0a624380cd2c9
fukunagaa <k.k.0627ku@gmail.com>
2020-10-13 22:55:25 +0900

    sample 更新

--- webpack-sample/src/sample.ts
+++ webpack-sample/src/sample.ts
@@ -1,3 +1,9 @@
+function dummy(str: string) {
+  console.log("dummy:" + str);
+}
+
 export function sample1() {
-  console.log("sample log");
+  const str = "mojimoji";
+  console.log("sample " + str);
+  dummy("sample1 call");
 }

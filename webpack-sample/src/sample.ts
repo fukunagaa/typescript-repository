99ef2273abe25cfa741475b9d4e606c6185b9a1c
fukunagaa <k.k.0627ku@gmail.com>
2020-10-13 23:04:06 +0900

    square 追加

--- webpack-sample/src/sample.ts
+++ webpack-sample/src/sample.ts
@@ -2,8 +2,14 @@ function dummy(str: string) {
   console.log("dummy:" + str);
 }
 
+function square(num: number): number {
+  return num * num;
+}
+
 export function sample1() {
   const str = "mojimoji";
   console.log("sample " + str);
   dummy("sample1 call");
+  let sq = square(5);
+  console.log(sq);
 }

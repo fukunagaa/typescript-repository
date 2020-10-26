6a558243503d29f347b4768c4f4d2150dbb6a1b9
fukunagaa <k.k.0627ku@gmail.com>
2020-10-26 21:36:09 +0900

    react components作成

--- webpack-react/src/index.tsx
+++ webpack-react/src/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import App from "./App";
+
 ReactDOM.render(
-    <h1>Hello React!</h1>,
-    document.getElementById('app')
+  <div>
+    <h1>Hello React!</h1>
+    <App />
+  </div>,
+  document.getElementById('app')
 );

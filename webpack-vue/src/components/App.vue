83e09c936305507c43496c23a844cd86694560ef
fukunagaa <k.k.0627ku@gmail.com>
2020-10-27 21:44:56 +0900

    v-if v-for 使用例

--- webpack-vue/src/components/App.vue
+++ webpack-vue/src/components/App.vue
@@ -1,13 +1,27 @@
 <template>
   <div>
     <p>{{ message }}</p>
+    <span v-if="seen">Now you see me</span>
+    <ul>
+      <li v-for="(todo, key) in todos" :key="key">
+        {{ todo.text }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return { message: "success" };
+  data: function() {
+    return { 
+      message: "success",
+      seen: true,
+      todos: [
+        {text: "掃除"},
+        {text: "洗濯"},
+        {text: "食事"}
+      ]
+    };
   },
 };
 </script>

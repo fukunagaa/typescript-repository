affa76481126d9a09b25914a4bdd35c79407b56f
fukunagaa <k.k.0627ku@gmail.com>
2020-10-28 23:01:09 +0900

    算出プロパティ getter setter定義

--- webpack-vue/src/components/App.vue
+++ webpack-vue/src/components/App.vue
@@ -13,6 +13,10 @@
     <ul>
       <TodoItem v-for="todo in todos" v-bind:todo="todo" v-bind:key="todo.id"></TodoItem>
     </ul>
+    <div>
+      <input v-model="fullName">
+      {{ fullName }}
+    </div>
   </div>
 </template>
 
@@ -29,12 +33,28 @@ export default {
         {id:1, text: "掃除"},
         {id:2, text: "洗濯"},
         {id:3, text: "食事"}
-      ]
+      ],
+      firstName: "aa",
+      lastName: "bb"
     };
   },
+  // 算出プロパティはリアクティブな依存関係にもとづきキャッシュされる
+  // messageに変更がない限り再計算は行わない
   computed: {
     reversedMessage: function() {
       return this.message.split("").reverse().join("");
+    },
+    fullName:{
+      // getter 関数
+      get: function () {
+        return this.firstName + ' ' + this.lastName
+      },
+      // setter 関数
+      set: function (newValue) {
+        var names = newValue.split(' ')
+        this.firstName = names[0]
+        this.lastName = names[names.length - 1]
+      }
     }
   },
   components: {
